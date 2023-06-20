72a7e784d17db9e4e5b425b04c75e80de6643ef2
Rajasekhar Gajula <rajasekhar.gajula@accenture.com>
2023-06-21 00:05:00 +0800

    fin

---
+++ api_helper.py
@@ -0,0 +1,29 @@
+
+from threading import Timer
+import pandas as pd
+import time
+import sys
+import subprocess
+
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "./NorenRestApiPy-0.0.16-py2.py3-none-any.whl"])
+from NorenRestApiPy.NorenApi import  NorenApi
+
+class SymbolItem:
+    def __init__(self):
+        self.df = None
+        self.key = None
+        self.counter  = 0
+        self.lasttime = 0    
+
+def get_time(time_string):
+    data = time.strptime(time_string,'%d-%m-%Y %H:%M:%S')
+
+    return time.mktime(data)
+
+
+class ShoonyaApiPy(NorenApi):
+    def __init__(self):
+        NorenApi.__init__(self, host='https://api.shoonya.com/NorenWClientTP/', websocket='wss://api.shoonya.com/NorenWSTP/', eodhost='https://api.shoonya.com/chartApi/getdata/')
+
+    
+    
--- app.py
+++ app.py
@@ -1,10 +1,56 @@
 import os
-
+from api_helper import ShoonyaApiPy
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+from api_helper import ShoonyaApiPy
+from NorenRestApiPy.NorenApi import NorenApi
+import pyotp
+import traceback
+import warnings
 
 app = Flask(__name__)
 
+uid = str('FA75410')
+pwd = str('Nar7@198983')
+factor2 = str('15-04-1983')
+vc = str('FA75410_U')
+app_key = str('29ae32c0f6585938605c4ca65258fdd8')
+imei = str('abc1234')
+totptoken = 'LT4742H72JRUS5H27442C426PB2747G7'
+totp = pyotp.TOTP(totptoken).now()
+print(totp)
+FinvasiaClient = ShoonyaApiPy()
+def FinvasiaLogin():
+    try:
+        global FinvasiaClient
+        global isTeleError
+        #time.sleep(60)
+        #start of our program
+
+        FinvasiaClient.login(userid=uid,
+                             password=pwd,
+                             twoFA=totp,
+                             vendor_code=vc,
+                             api_secret=app_key,
+                             imei=imei)
+
+        if FinvasiaClient is not None:
+            print("Login Success....")
+            #print("I am alive:....Waiting for Telegram Signal....")
+            return "Login Success...."
+        else:
+            print("Login failed1, algo restarting....")
+            FinvasiaLogin()
+            return "Login failed2, app restarting...."
+    except:
+        print("Login failed3, algo restarting....")
+        print(traceback.format_exc())
+
+        FinvasiaLogin()
+        pass
+
+        return "Login failed4, algo restarting...."
+FinvasiaLogin()
 
 @app.route('/')
 def index():
@@ -18,12 +64,13 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
-
-   if name:
+    
+    name = request.form.get('name')
+    
+    if name:
        print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name)
-   else:
+    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
