d968501a67207b2a7ccc6fe0af0811473d3734b6
Rajasekhar <rajamca7@gmail.com>
2023-07-04 10:35:43 +0800

    1

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ from NorenRestApiPy.NorenApi import NorenApi
 import pyotp
 import traceback
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from telethon import TelegramClient, events
+import gspread
+import pytz
 #import gunicorn
 username = ''
 raja_username = ''
@@ -40,8 +44,33 @@ totptoken = 'LT4742H72JRUS5H27442C426PB2747G7'
 totp = pyotp.TOTP(totptoken).now()
 print(totp)
 
-FINTradeAt = 19750
-FINsymbol = "FINNIFTY04JUL23"
+telekey = '5551030547:AAH6pQELunbkTCwGm4O4O372XQXOTRZvEjg'
+client = TelegramClient('RajaTeleAlgo', '7722627','a83468f1a7a200a3fa28672ef1feb7c9')
+
+
+myscope = ['https://spreadsheets.google.com/feeds', 
+            'https://www.googleapis.com/auth/drive']
+
+mycred = ServiceAccountCredentials.from_json_keyfile_name('hlprj-377707-9d9ee1b7035e.json',myscope)
+
+wsclient =gspread.authorize(mycred)
+#ws = client.open("RJTRADE").sheet1
+wb = wsclient.open_by_key('1Ensy2EbpfrP7ol8KEHgVcihToqD7aYtcbpSJpZiBK8Y')
+
+ws = wb.worksheet('Data')
+BNsymbol = str(ws.cell(2, 1).value)
+FINsymbol = str(ws.cell(3, 1).value)
+totalpremium = float(ws.cell(6, 1).value)
+wsbntradeat = str(ws.cell(4, 1).value)
+wsfintradeat = str(ws.cell(5, 1).value)
+print(BNsymbol)
+print(FINsymbol)
+print(wsbntradeat)
+print(wsfintradeat)
+isstoptrade = False
+IST = pytz.timezone('Asia/Kolkata')
+
+
 
 FinvasiaClient = ShoonyaApiPy()
 def FinvasiaLogin():
@@ -77,8 +106,8 @@ def FinvasiaLogin():
 FinvasiaLogin()
 
 FINnumarraystk = []
-FINfirststk = int(FINTradeAt) - 500
-while FINfirststk != int(FINTradeAt) + 500:
+FINfirststk = int(wsfintradeat) - 500
+while FINfirststk != int(wsfintradeat) + 500:
 
     FINpestk = FinvasiaClient.searchscrip(exchange="NFO",
                                             searchtext=FINsymbol + 'P' +
@@ -199,8 +228,8 @@ def getprofitraja75410():
     global BankNiftyIndex
     global FINiftyIndex
     all_strikes = []
-    FINCESTRIKEATMAt = int(FINTradeAt) + 50
-    FINPESTRIKEATMAt = int(FINTradeAt) - 50
+    FINCESTRIKEATMAt = int(wsfintradeat)
+    FINPESTRIKEATMAt = int(wsfintradeat)
     FINactivestik_p = ''
     all_strikes.append('26009')
     all_strikes.append('26037')
