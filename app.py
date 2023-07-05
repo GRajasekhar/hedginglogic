e0fd8240b527b3f4f7872eb98faf7a5750a3a392
Rajasekhar <rajamca7@gmail.com>
2023-07-05 12:53:03 +0800

    2

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import time
 
 telekey = '5551030547:AAH6pQELunbkTCwGm4O4O372XQXOTRZvEjg'
-client = TelegramClient('RajaTeleAlgo', '7722627','a83468f1a7a200a3fa28672ef1feb7c9')
+#client = TelegramClient('RajaTeleAlgo', '7722627','a83468f1a7a200a3fa28672ef1feb7c9')
 
 
 myscope = ['https://spreadsheets.google.com/feeds', 
@@ -155,9 +155,9 @@ def favicon():
 
 def update_text_background_task():
     with app.app_context():
-        background_thread = threading.Thread(target=priceupdate.getdata)
-        background_thread.daemon = True  
-        background_thread.start()
+        #background_thread = threading.Thread(target=priceupdate.getdata)
+        #background_thread.daemon = True  
+        #background_thread.start()
         while True:
             # Perform any necessary data updates here
             updated_text = 'Updated text: ' + str(time.time())
